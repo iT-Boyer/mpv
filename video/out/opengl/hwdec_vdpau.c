792bb30b0b3493519f4e134d0d93a0f7a6b754fb
wm4 <wm4@nowhere>
2016-06-08 22:39:39 +0200

    wip: map vdpau video surfaces directly
    Instead of converting to RGB and then mapping the RGB surface, map the
    video surfaces directly.
    
    TODO:
    - deinterleave the frames
    - apply this only to nv12 surfaces (not 422-as-nv12)
    - also disable it if --vf=vdpaupp processing is used (deint etc.)

--- video/out/opengl/hwdec_vdpau.c
+++ video/out/opengl/hwdec_vdpau.c
@@ -36,10 +36,11 @@ struct priv {
     struct mp_vdpau_ctx *ctx;
     uint64_t preemption_counter;
     struct mp_image_params image_params;
-    GLuint gl_texture;
+    GLuint gl_textures[4];
     bool vdpgl_initialized;
     GLvdpauSurfaceNV vdpgl_surface;
     VdpOutputSurface vdp_surface;
+    GLvdpauSurfaceNV vdpgl_video_surface;
     struct mp_vdpau_mixer *mixer;
     bool mapped;
 };
@@ -49,8 +50,14 @@ static void unmap(struct gl_hwdec *hw)
     struct priv *p = hw->priv;
     GL *gl = hw->gl;
 
-    if (p->mapped)
-        gl->VDPAUUnmapSurfacesNV(1, &p->vdpgl_surface);
+    if (p->mapped) {
+        //gl->VDPAUUnmapSurfacesNV(1, &p->vdpgl_surface);
+        if (p->vdpgl_video_surface) {
+            gl->VDPAUUnmapSurfacesNV(1, &p->vdpgl_video_surface);
+            gl->VDPAUUnregisterSurfaceNV(p->vdpgl_video_surface);
+            p->vdpgl_video_surface = 0;
+        }
+    }
     p->mapped = false;
 }
 
@@ -75,8 +82,8 @@ static void destroy_objects(struct gl_hwdec *hw)
         gl->VDPAUUnregisterSurfaceNV(p->vdpgl_surface);
     p->vdpgl_surface = 0;
 
-    glDeleteTextures(1, &p->gl_texture);
-    p->gl_texture = 0;
+    glDeleteTextures(4, p->gl_textures);
+    memset(p->gl_textures, 0, sizeof(p->gl_textures));
 
     if (p->vdp_surface != VDP_INVALID_HANDLE) {
         vdp_st = vdp->output_surface_destroy(p->vdp_surface);
@@ -156,25 +163,30 @@ static int reinit(struct gl_hwdec *hw, struct mp_image_params *params)
                                         params->w, params->h, &p->vdp_surface);
     CHECK_VDP_ERROR(p, "Error when calling vdp_output_surface_create");
 
-    gl->GenTextures(1, &p->gl_texture);
-    gl->BindTexture(GL_TEXTURE_2D, p->gl_texture);
-    gl->TexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    gl->TexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    gl->TexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    gl->TexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    gl->GenTextures(4, p->gl_textures);
+    for (int n = 0; n < 4; n++) {
+        gl->BindTexture(GL_TEXTURE_2D, p->gl_textures[n]);
+        gl->TexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        gl->TexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        gl->TexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        gl->TexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    }
     gl->BindTexture(GL_TEXTURE_2D, 0);
 
+    /*
     p->vdpgl_surface = gl->VDPAURegisterOutputSurfaceNV(BRAINDEATH(p->vdp_surface),
                                                         GL_TEXTURE_2D,
-                                                        1, &p->gl_texture);
+                                                        1, p->gl_textures);
     if (!p->vdpgl_surface)
         return -1;
 
     gl->VDPAUSurfaceAccessNV(p->vdpgl_surface, GL_READ_ONLY);
+    */
 
     gl_check_error(gl, hw->log, "After initializing vdpau OpenGL interop");
 
-    params->imgfmt = IMGFMT_RGB0;
+    //params->imgfmt = IMGFMT_RGB0;
+    params->imgfmt = IMGFMT_NV12;
 
     return 0;
 }
@@ -184,6 +196,8 @@ static int map_frame(struct gl_hwdec *hw, struct mp_image *hw_image,
 {
     struct priv *p = hw->priv;
     GL *gl = hw->gl;
+    struct vdp_functions *vdp = &p->ctx->vdp;
+    VdpStatus vdp_st;
 
     int pe = mp_vdpau_handle_preemption(p->ctx, &p->preemption_counter);
     if (pe < 1) {
@@ -194,23 +208,48 @@ static int map_frame(struct gl_hwdec *hw, struct mp_image *hw_image,
             return -1;
     }
 
+    /*
     if (!p->vdpgl_surface)
         return -1;
 
     mp_vdpau_mixer_render(p->mixer, NULL, p->vdp_surface, NULL, hw_image, NULL);
 
     gl->VDPAUMapSurfacesNV(1, &p->vdpgl_surface);
+    */
+
+    VdpVideoSurface surface = (intptr_t)hw_image->planes[3];
+
+    VdpChromaType s_chroma_type;
+    uint32_t s_w, s_h;
+
+    vdp_st = vdp->video_surface_get_parameters(surface, &s_chroma_type, &s_w, &s_h);
+    CHECK_VDP_ERROR(hw, "Error when calling vdp_video_surface_get_parameters");
+
+    p->vdpgl_video_surface = gl->VDPAURegisterVideoSurfaceNV(BRAINDEATH(surface),
+                                                             GL_TEXTURE_2D,
+                                                             4, p->gl_textures);
+    if (!p->vdpgl_video_surface)
+        return -1;
+
+    gl->VDPAUSurfaceAccessNV(p->vdpgl_video_surface, GL_READ_ONLY);
+    gl->VDPAUMapSurfacesNV(1, &p->vdpgl_video_surface);
+
     p->mapped = true;
     *out_frame = (struct gl_hwdec_frame){
-        .planes = {
-            {
-                .gl_texture = p->gl_texture,
-                .gl_target = GL_TEXTURE_2D,
-                .tex_w = p->image_params.w,
-                .tex_h = p->image_params.h,
-            },
-        },
+        .interlaced = true,
     };
+    for (int n = 0; n < 4; n++) {
+        bool chroma = n >= 2;
+        out_frame->planes[n] = (struct gl_hwdec_plane){
+            .gl_texture = p->gl_textures[n],
+            .gl_target = GL_TEXTURE_2D,
+            .tex_w = s_w / (chroma ? 2 : 1),
+            .tex_h = s_h / (chroma ? 4 : 2),
+        };
+    };
+    // as a temporary hack: change it from 0/1=luma0/1 & 1/2=chroma0/1 to
+    // 0/1=chroma0/luma0 & 1/2=chroma1/luma1
+    MPSWAP(struct gl_hwdec_plane, out_frame->planes[1], out_frame->planes[2]);
     return 0;
 }
 
